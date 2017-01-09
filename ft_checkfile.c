7e958c40a0abf567c01f2cf6963ece87119aed72
Maxime GHAZARIAN <mghazari@e1r9p3.42.fr>
2017-01-09 14:05:54 +0100

    progress

--- ft_checkfile.c
+++ ft_checkfile.c
@@ -6,7 +6,7 @@
 /*   By: mghazari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 11:55:42 by mghazari          #+#    #+#             */
-/*   Updated: 2017/01/09 13:30:13 by mghazari         ###   ########.fr       */
+/*   Updated: 2017/01/09 14:04:44 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,25 @@
 int	check_file(const char *str)
 {
 	int	i;
-	int j;
+	int	t;
 
 	i = -1;
-	j = 0;
+	t = 1;
 	while (str[++i])
 	{
-		if (str[i] != '.' || str[i] != '#' || str[i] != '\n')
+		if ((str[i] != '.' && str[i] != '#' && str[i] != '\n') \
+				|| (i % 4 == 0 && str[i] != '\n')\
+				|| (str[i] == '\n' && i % 4 != 0))
 			return (0);
-		if (i % 4 == 0 && str[i] != '\n')
-			return (0);
-		if (str[i] == '\n' && i % 4 != 0)
-			return (0);
-		//TODO
+		if (str[i] == '\n')
+			if (str[i + 1] == '\0')
+				return (++t);
+			else if (str[i + 1] == '\n')
+			{
+				t++;
+				i++;
+			}
+			else
+				return (0);
 	}
 }
