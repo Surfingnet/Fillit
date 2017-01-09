5cf94bf48f43e8b6e47b69c77e531c090b93f416
Julien GAILLARD <jgaillar@e1r9p2.42.fr>
2017-01-09 11:50:02 +0100

    buf_size .h

--- fillit.h
+++ fillit.h
@@ -6,13 +6,13 @@
 /*   By: jgaillar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/04 15:24:17 by jgaillar          #+#    #+#             */
-/*   Updated: 2017/01/08 10:31:04 by mghazari         ###   ########.fr       */
+/*   Updated: 2017/01/09 11:49:51 by jgaillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
-
+# define BUFF_SIZE 4096
 #include "libft.h"
 #include <fcntl.h>
 
