cb6252a84c9351cadf5bf1ae993d37d20582c6b8
Surfingnet <maximeghazarian1@gmail.com>
2017-01-10 12:10:43 +0100

    fixingmain

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jgaillar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/04 15:25:56 by jgaillar          #+#    #+#             */
-/*   Updated: 2017/01/09 16:47:20 by jgaillar         ###   ########.fr       */
+/*   Updated: 2017/01/10 12:00:12 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@
 
 int	main(int argc, char *argv[])
 {
-	char **tab;
-	char *s;
-	int i;
+	char	**tab;
+	char	*s;
+	int	i;
+	int	t;
 
 	i = 0;
-	s = ft_readfile(ft_openfile(argv[1]));
 	if (argc != 2)
-		return (-1);
-	//ft_readfile(ft_openfile(argv[1]));
-	tab = (ft_putintab(s, ft_checkfile(s)));
-	while (tab[i])
+		return (1);
+	if (!(s = ft_readfile(ft_openfile(argv[1]))) || !(t = ft_checkfile(s)))
 	{
-		ft_putendl(tab[i]);
-		i++;
+		ft_putendl("error");
+		return (1);
 	}
+	tab = (ft_putintab(s, t));
+	/*while (tab[i])
+	  {
+	  ft_putendl(tab[i]);
+	  i++;
+	  }
+	 */
 	return (0);
 }
