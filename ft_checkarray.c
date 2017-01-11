ba56ca193170ab32e4d8078c63f559ab1e504dc2
Maxime GHAZARIAN <mghazari@e1r10p3.42.fr>
2017-01-11 16:55:07 +0100

    fixing malloc segfault on 1 tetri entry

--- ft_checkarray.c
+++ ft_checkarray.c
@@ -6,7 +6,7 @@
 /*   By: mghazari <mghazari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 13:38:34 by mghazari          #+#    #+#             */
-/*   Updated: 2017/01/10 17:12:22 by mghazari         ###   ########.fr       */
+/*   Updated: 2017/01/11 16:54:11 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,25 @@ static int	count_conn(char *tetri[], int i, int j)
 {
 	int connections;
 
+	ft_putstr("counting connections on ");
+	ft_putnbr(i);
+	ft_putchar(',');
+	ft_putnbr(j);
+	ft_putstr(": ");
+
 	connections = 0;
-	if ((i - 1) > -1 && tetri[i - 1][j] == '#')
+	if ((i - 1) > -1 && tetri[i - 1][j] != '.')
 		connections++;
-	if ((i + 1) < 4 && tetri[i + 1][j] == '#')
+	if ((i + 1) < 4 && tetri[i + 1][j] != '.')
 		connections++;
-	if ((j - 1) > -1 && tetri[i][j - 1] == '#')
+	if ((j - 1) > -1 && tetri[i][j - 1] != '.')
 		connections++;
-	if ((j + 1) < 4 && tetri[i][j + 1] == '#')
+	if ((j + 1) < 4 && tetri[i][j + 1] != '.')
 		connections++;
+	
+	ft_putnbr(connections);
+	ft_putendl("\n");
+	
 	return (connections);
 }
 
@@ -34,15 +44,22 @@ static int	connected(char *tetri[])
 	int	i;
 	int	j;
 
+	ft_putendl("\nstarting connection count-\n");
+
 	total = 0;
 	i = -1;
 	while (tetri[++i])
 	{
 		j = -1;
 		while (tetri[i][++j])
-			if (tetri[i][j] == '#')
+			if (tetri[i][j] != '.')
 				total += count_conn(tetri, i, j);
 	}
+
+	ft_putstr("\ncount ended, total: ");
+	ft_putnbr(total);
+	ft_putendl("\n============================\n");
+
 	return (total == 6 || total == 8);
 }
 
@@ -58,7 +75,7 @@ static int	four_blocks(char *tetri[])
 	{
 		j = -1;
 		while (tetri[i][++j])
-			if (tetri[i][j] == '#')
+			if (tetri[i][j] != '.')
 				f++;
 	}
 	return (f == 4);
@@ -70,7 +87,12 @@ int		ft_checkarray(char **array[])
 
 	i = -1;
 	while (array[++i])
+	{
+		ft_putstr("\nchecking tetri nb: ");
+		ft_putnbr(i);
+		ft_putendl("\n");
 		if(!(four_blocks(array[i]) && connected(array[i])))
 			return (0);
+	}
 	return (1);
 }
--- ft_putintab.c
+++ ft_putintab.c
@@ -6,7 +6,7 @@
 /*   By: jgaillar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 16:41:30 by jgaillar          #+#    #+#             */
-/*   Updated: 2017/01/09 16:41:47 by jgaillar         ###   ########.fr       */
+/*   Updated: 2017/01/11 16:51:35 by mghazari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,15 @@ char	***memtab(int len)
 	int i;
 	int j;
 
-	i = 0;
+	i = -1;
 	j = 0;
 
 	if (!(tab = (char ***)malloc(sizeof(char **) * (len + 1))))
 		return (NULL);
-	tab[len + 1] = NULL;
-	while (tab[i])
-	{
-
+	tab[len] = NULL;
+	while (tab[++i])
 		if (!(tab[i] = (char **)malloc(sizeof(char *) * (5))))
 			return (NULL);
-		i++;
-	}
 	return (tab);
 }
 
@@ -38,18 +34,15 @@ char	***ft_putintab(char *s, int len)
 {
 	char ***tab;
 	int i;
-	int j;
 
-	i = 0;
-	j = 0;
+	i = -1;
 
 	if (!(tab = memtab(len)))
 		return (NULL);
-	while (i < len)
+	while (++i < len)
 	{
 		if (!(tab[i] = ft_strsplit(ft_strsub(s, i * 21, 21), '\n')))
 			return (NULL);
-		i++;
 	}
 	return (tab);
 }
